785872e0a159c531fe70a632ccf7ba90c5442479
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-06-09 19:12:31 +0200

    CSS Animation
    Creating a Slide Transition with the CSS Animation Property

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,9 @@
             *-enter-active
             *-leave
             *-leave-active -->
+            <transition name="slide">
+                <div class="alert alert-info" v-if="show">This is some info</div>
+            </transition>
             
         </div>
     </div>
@@ -51,5 +54,33 @@ export default {
     .fade-leave-active {
         transition: opacity 1s;
         opacity: 0;
+    }
+    .slide-enter {
+        /* starting state is set in keyframe */
+    }
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+    }
+    .slide-leave {
+        /* starting state is where the element now is*/
+    }
+    .slide-leave-active {
+        animation: slide-out 1s ease-out forwards;
+    }
+    @keyframes slide-in {
+        from {
+            transform: translateY(20px);
+        }
+        to {
+            transform: translateY(0);
+        }
+    }
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+        }
+        to {
+            transform: translateY(20px);
+        }
     }
 </style>

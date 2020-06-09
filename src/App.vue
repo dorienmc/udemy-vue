ac6f17014e6d542ea23d6bf47d0dfe493e127671
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-06-09 18:32:04 +0200

    Creating a  Transition with the CSS Transition Property

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
     <div class="container">
         <div class="row">
-        
+            <h1>Animations</h1>
+            <hr>
+            <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+            <br><br>
+            <!-- Will only show on element at a time -->
+            <transition name="fade">
+                <div class="alert alert-info" v-if="show">This is some info</div>
+            </transition>
+            <!-- CSS for created/removed elements (* == transition name or v) -->
+            <!-- *-enter     initial state
+            *-enter-active
+            *-leave
+            *-leave-active -->
+            
         </div>
     </div>
 </template>
@@ -11,6 +24,7 @@
 export default {
     data() {
         return {
+           show: false
         }
     },
     computed: {
@@ -25,4 +39,17 @@ export default {
 </script>
 
 <style>
+    .fade-enter {
+        opacity: 0;
+    }
+    .fade-enter-active {
+        transition: opacity 1s; /* transition to default, which is opacity 1*/
+    }
+    .fade-leave {
+         /* opacity: 1; Not needed, is the default*/
+    }
+    .fade-leave-active {
+        transition: opacity 1s;
+        opacity: 0;
+    }
 </style>

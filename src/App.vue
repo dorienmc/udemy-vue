4c5ca06fd4bf73b4dcd1b65b1de0bde58855835e
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-06-09 18:14:38 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row">
+        
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+        }
+    },
+    computed: {
+        
+    },
+    filters: {
+        
+    },
+    components: {
+    }
+}
+</script>
+
+<style>
+</style>

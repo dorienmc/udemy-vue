f8ea6951ac4df2448237901d675067f7549bbc94
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-06-17 09:33:30 +0200

    Intercept requests

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import VueResource from 'vue-resource'
 Vue.use(VueResource);
 //Global config of vue-resource
 Vue.http.options.root = 'https://udemy-vuejs-http-45e40.firebaseio.com/data.json';
+//Interceptors
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if(request.method == 'POST') { 
+    request.method = 'PUT';
+  }
+  next();
+});
 
 new Vue({
   el: '#app',

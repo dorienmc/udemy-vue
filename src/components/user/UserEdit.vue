db0c24f8daa4fc4d8f50361ba5281f299bd8ae41
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-06-18 12:15:22 +0200

    Query parameters

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -2,5 +2,7 @@
     <div>
     <h3>Edit the User</h3>
     <p> ID: {{ $route.params.id }} </p>
+    <p> Locale: {{ $route.query.locale}} </p>
+    <p> Query #: {{ $route.query.q}} </p>
     </div>
 </template>

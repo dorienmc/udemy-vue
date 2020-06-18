7dceb1cdb7992a75e05a04283fb6e2b635eab457
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-06-18 10:03:41 +0200

    Route parameters

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <ul class="nav nav-pills">
     <router-link to="/" tag="li" active-class="active" exact><a href="">Home</a></router-link>
-    <router-link to="/user" tag="li" active-class="active"><a href="">User</a></router-link>
+    <router-link to="/user/1" tag="li" active-class="active"><a href="">User 1</a></router-link>
+    <router-link to="/user/2" tag="li" active-class="active"><a href="">User 2</a></router-link>
   </ul>
 </template>

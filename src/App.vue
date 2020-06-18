fa62e6aa3ad72900ca73ffd513dd94edc1d97573
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-06-18 08:56:25 +0200

    Setup section 16 Routing

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="container">
         <div class="row">
-        
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Routing</h1>
+            </div>
         </div>
     </div>
 </template>
@@ -12,14 +14,6 @@ export default {
     data() {
         return {
         }
-    },
-    computed: {
-        
-    },
-    filters: {
-        
-    },
-    components: {
     }
 }
 </script>

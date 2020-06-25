56873feffe3222852e4883c367bcbd56b2d5cda8
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-06-25 10:14:33 +0200

    Mapping getters to properties
    And using the spread operator (needs babel stage-2)

--- src/components/AnotherResult.vue
+++ src/components/AnotherResult.vue
@@ -1,13 +1,30 @@
 <template>
-  <p>Counter is: {{ counter }}</p>  
+  <div>
+    <p>Counter is: {{ doubleCounter }}</p>
+    <p>Number of Clicks: {{ stringCounter }}</p>
+  </div>
+  
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
-  computed: {
-    counter() {
-      return this.$store.getters.doubleCounter;
+  computed: { 
+    //... (spread operator, ES6) pull out all properties and methods and creates seperate key value pairs for each of them
+    ...mapGetters([
+      'doubleCounter', 
+      'stringCounter'
+    ]),
+    ourOwnComputedProperty() {
+
     }
+    // counter() {
+    //   return this.$store.getters.doubleCounter;
+    // },
+    // clicks() {
+    //   return this.$store.getters.stringCounter;
+    // }
   }
 }
 </script>

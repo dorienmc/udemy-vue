4acdf86ab408a20d9e700f3c7807ac91968abc89
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-06-26 08:53:13 +0200

    Improving folder structures (p1)

---
+++ src/store/modules/counter.js
@@ -0,0 +1,47 @@
+const state = {
+  counter: 0
+}
+
+const getters = {
+  doubleCounter: (state) => {
+    return state.counter * 2;
+  },
+  stringCounter: state => {
+    return state.counter + ' Clicks';
+  }
+}
+
+const mutations = {
+  increment: (state, payload) => {
+    state.counter += payload;
+  },
+  decrement: state => {
+    state.counter--;
+  }
+}
+
+const actions= {
+  increment: ({ commit }, payload) => {
+    commit('increment', payload);
+  },
+  decrement: ({ commit }) => { //If you only need the commit part you can do it like this
+    commit('decrement');
+  },
+  asyncIncrement: ({ commit }, payload) => {
+    setTimeout(() => {
+      commit('increment', payload.duration);
+    }, payload.duration);
+    },
+  asyncDecrement: ({ commit }) => {
+    setTimeout(() => {
+      commit('decrement');
+    }, 1000);
+  }
+}
+
+export default {
+  state, //es6 for  "state: state"
+  getters,
+  mutations,
+  actions
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,55 +1,30 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import counter from './modules/counter.js';
 
 //Register plugin
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
-    counter: 0,
     value: 0
   },
   getters: {
-    doubleCounter: (state) => {
-      return state.counter * 2;
-    },
-    stringCounter: state => {
-      return state.counter + ' Clicks';
-    },
     value: state => {
       return state.value;
     }
   },
   mutations: {
-    increment: (state, payload) => {
-      state.counter += payload;
-    },
-    decrement: state => {
-      state.counter--;
-    },
     updateValue: (state, payload) => {
       state.value = payload;
     }
   },
   actions: {
-    increment: ({ commit }, payload ) => {
-      commit('increment', payload);
-    },
-    decrement: ({commit}) => { //If you only need the commit part you can do it like this
-      commit('decrement');
-    },
-    asyncIncrement: ({commit}, payload) => {
-      setTimeout(() => {
-        commit('increment',payload.duration);
-      }, payload.duration);
-    },
-    asyncDecrement: ({ commit }) => {
-      setTimeout(() => {
-        commit('decrement');
-      }, 1000);
-    },
     updateValue: ({ commit }, payload) => {
       commit('updateValue', payload);
     }
+  },
+  modules: {
+    counter
   }
 });

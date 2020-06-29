ead4954ccf9b8d388dcaf815ef9dc99f6a9aeb92
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-06-29 16:17:43 +0200

    Show all users
    Add some bootstrap cards

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -2,16 +2,45 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
+    <p>Your email address: {{ email }}</p>
+
+    <hr>
+    <div class="row" style="padding:50px"> 
+      <app-user v-for="user in users" :user=user :key=user.id></app-user>
+    </div>
+    
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import user from './user.vue';
 
 export default {
+  data() {
+    return {
+      email: '',
+      users: []
+    }
+  },
+  components: {
+    appUser: user
+  },
   created() {
     axios.get('https://vuejs-udemy-axios-d6bf5.firebaseio.com/users.json')
-      .then(response => console.log(response))
+      .then(response => {
+        console.log(response)
+        const data = response.data
+        const users = []
+        for (let key in data) {
+          const user = data[key]
+          user.id = key
+
+          users.push(user)
+        }
+        this.users = users
+        this.email = users[0].email
+      })
       .catch(error => console.error(error))
   }
 }
@@ -25,4 +54,5 @@ export default {
   p {
     color: red;
   }
+
 </style>
---
+++ src/components/dashboard/user.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="col-sm-6">
+  <div class="card bg-light mb-3"" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">{{ user.email}}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">{{user.country}}</h6>
+      <p class="card-text" v-for="hobby in user.hobbyInput" :key=hobby>{{hobby}}</p>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['user']
+}
+</script>
+
+<style>
+
+</style>

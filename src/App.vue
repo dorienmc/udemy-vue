cac35ba5eb1c65b87e20cacaf8e36b962cb1386e
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-06-29 15:49:43 +0200

    setup app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-    <div class="container">
-        <div class="row">
-        
-        </div>
-    </div>
+  <div id="app">
+    <app-header />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-
-export default {
-    data() {
-        return {
-        }
-    },
-    computed: {
-        
-    },
-    filters: {
-        
-    },
+  import Header from './components/header/header.vue'
+  export default {
+    name: 'app',
     components: {
+      'app-header': Header
     }
-}
+  }
 </script>
 
 <style>
+  body, html {
+    margin: 0;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
 </style>

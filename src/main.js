0954b322e908881969cc70603090933c57c4af30
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-06-29 16:31:21 +0200

    Using interceptors

--- src/main.js
+++ src/main.js
@@ -7,7 +7,22 @@ import store from './store'
 
 axios.defaults.baseURL = "https://vuejs-udemy-axios-d6bf5.firebaseio.com";
 axios.defaults.headers.common['Authorization']='some_auth_token';
-axios.defaults.headers.get['Accepts']='application/json'
+axios.defaults.headers.get['Accepts']='application/json';
+const requestInterceptor = axios.interceptors.request.use(config => {
+  console.log("Request interceptor", config)
+  //do stuff
+  return config
+});
+const responseInterceptor = axios.interceptors.response.use(response => {
+  console.log("Response interceptor", response)
+  //do stuff
+  return response;
+});
+
+//remove interceptors
+axios.interceptors.request.eject(requestInterceptor);
+axios.interceptors.request.eject(responseInterceptor);
+
 
 new Vue({
   el: '#app',

c8be113ef87a2133d9d9da3ee2b654913a6d1e9f
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-06-29 13:22:22 +0200

    Setup vue-resource

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 
 import App from './App.vue'
 import { routes } from './routes';
@@ -14,6 +15,9 @@ const router = new VueRouter({
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString();
 });
+
+Vue.use(VueResource);
+Vue.http.options.root = "https://vue-js-udemy-stock-trader.firebaseio.com/";
 
 new Vue({
   el: '#app',

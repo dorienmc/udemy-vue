5e8854f7c901a8f51162991741ff6b8374784b58
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-06-29 12:17:30 +0200

    save and fetch data

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,18 +13,19 @@
           <strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
           <ul class="nav navbar-nav navbar-right">
             <li><a href="#" @click="endDay">End Day</a></li>
-            <li class="dropdown">
+            <li class="dropdown" @click="isDropdownOpen = !isDropdownOpen">
               <a href="#" 
               class="dropdown-toggle"
               data-toggle="dropdown"
               role="button"
               aria-haspopup="true"
               aria-expanded="false">Save & Load <span class="caret"></span></a>
+            
+              <ul class="dropdown-menu" :class="{show: isDropdownOpen}" >
+                <li><a href="#">Save Data</a></li>
+                <li><a href="#">Load Data</a></li>
+              </ul>
             </li>
-            <ul class="dropdown-menu">
-              <li><a href="#">Save Data</a></li>
-              <li><a href="#">Load Data</a></li>
-            </ul>
           </ul>
       </div>
     </div>
@@ -35,6 +36,11 @@
 import {mapActions} from 'vuex';
 
 export default {
+  data() {
+    return {
+      isDropdownOpen: false
+    }
+  },
   computed: {
     funds() {
       return this.$store.getters.funds;

d187777f7f9e8a37dba61b41d46a2c3f2701469e
Dorien Lorijn <dorien.meijercluwen@nedap.com>
2021-05-23 13:51:25 +0200

    Event Modifiers

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      confirmedName: "",
     };
   },
   methods: {
+    submitForm(event) {
+      alert("Submitted");
+    },
     setName(event, lastName) {
       this.name = event.target.value + " " + lastName;
+    },
+    confirmName() {
+      this.confirmedName = this.name;
     },
     add(num) {
       this.counter += num;

6b47881ed2c9c4d29071ba3e5c7ed0fe1a444d15
Dorien Lorijn <dorien.meijercluwen@nedap.com>
2021-05-23 13:40:43 +0200

    Events & Methods

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
+  },
+  methods: {
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
+    add(num) {
+      this.counter += num;
+    },
+    remove(num) {
+      this.counter -= num;
+    },
   },
 });
 

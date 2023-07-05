848a0a975f07eeee8ba3158cf9168769f0f41fe7
Dorien Lorijn - Meijer Cluwen <dorien.meijercluwen@nedap.com>
2023-07-05 17:02:38 +0200

    Add vue app and use v-bind

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      courseGoal: "Learn Vue3",
+      vueLink: "https://vuejs.org/",
+    };
+  },
+  methods: {},
+});
+app.mount("#user-goal");
